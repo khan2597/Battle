ae6cc623dd4a3564d127f3aa31f7a89f72262a8b
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-29 15:40:33 +0000

    added tests for hit points

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,17 +2,34 @@ require 'player'
 
 describe Player do
     subject(:nate) { Player.new('Nate') }
+    subject(:jorge) { Player.new('Jorge') }
+
     it 'creates an instance of the Player class' do
         expect(subject).to be_an_instance_of(Player)
-    end
-
-    it 'starts with players having 100hp' do
-        expect(subject.health).to eq 100
     end
 
     describe '#name' do
         it 'returns the name' do
             expect(nate.name).to eq 'Nate'
+        end
+    end
+
+    describe '#hit_points' do
+        it 'returns the hit points' do
+            expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+        end
+    end
+
+    describe '#attack' do
+        it 'damages the player' do
+            expect(jorge).to receive(:receive_damage)
+            nate.attack(jorge)
+        end
+    end
+
+    describe '#receive_damage' do
+        it 'reduces the player hit points' do
+            expect { jorge.receive_damage }.to change { jorge.hit_points }.by(-10)
         end
     end
 end
