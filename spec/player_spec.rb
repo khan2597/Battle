3a683931e34e1a0befe4c1e16d5964ce7428aec5
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-29 15:09:14 +0000

    added test to take name paramater

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,14 @@
 require 'player'
 
 describe Player do
+    subject(:nate) { Player.new('Nate') }
     it 'creates an instance of the Player class' do
         expect(subject).to be_an_instance_of(Player)
+    end
+
+    describe '#name' do
+        it 'returns the name' do
+            expect(nate.name).to eq 'Nate'
+        end
     end
 end
