44eb91e1572b6fc980c7b60271275e4e7cf73dab
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-29 14:25:50 +0000

    added hit points tests and code

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,9 @@
-feature 'Enter names' do
-    scenario 'submitting names' do
+feature 'View hit points' do
+    scenario 'see Player 2 hit points' do
         visit('/')
         fill_in :player_1_name, with: 'Nate Diaz'
         fill_in :player_2_name, with: 'Jorge Masvidal'
         click_button 'Submit'
-        expect(page).to have_content 'Nate Diaz vs. Jorge Masvidal'
+        expect(page).to have_content 'Jorge Masvidal: 60HP'
     end
 end
