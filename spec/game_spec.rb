7e2fa5ec7ad0d14c76a7f16dffa53cf8434b6045
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-30 15:18:40 +0000

    refactored app.rb and game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do
-    subject(:game) { described_class.new }
     let(:player_1) { double :player }
     let(:player_2) { double :player }
+    subject(:game) { described_class.new(player_1, player_2) }
 
     it 'returns instance of game' do
         expect(subject).to be_an_instance_of(Game)
     end
 
+    describe '#player_1' do
+        it 'retrieves the first player' do
+            expect(game.player_1).to eq player_1
+        end
+    end
+
+    describe '#player_2' do
+        it 'retrieves the second player' do
+            expect(game.player_2).to eq player_2
+        end
+    end
+    
     describe '#attack' do
         it 'damages the player' do
             expect(player_2).to receive(:receive_damage)
