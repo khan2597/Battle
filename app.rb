5d30fc0ab0b8928fd7b3e22c332a3d53da00a60c
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-30 14:56:48 +0000

    added test for game class

--- app.rb
+++ app.rb
@@ -15,9 +15,8 @@ class Battle < Sinatra::Base
     end
 
     get '/play' do
-        @player_1_name = $player_1.name
-        @player_2_name = $player_2.name
-        @hit_point = $player_2.hit_points
+        @player_1 = $player_1
+        @player_2 = $player_2
         erb :play
     end
 
